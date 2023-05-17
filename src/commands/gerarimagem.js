9f6426346f90c3ba230abef884420e8af6e93055
Arthur <101161738+DarkBBR@users.noreply.github.com>
2023-05-17 00:23:08 -0300

    Add files via upload

---
+++ src/commands/gerarimagem.js
@@ -0,0 +1,78 @@
+const {
+  ApplicationCommandOptionType,
+  EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+} = require("discord.js");
+const { REPLICATE_API_KEY } = require("../../config.json");
+const models = require("../models");
+
+module.exports = {
+  run: async ({ interaction }) => {
+    try {
+      await interaction.deferReply();
+
+      const { default: Replicate } = await import("replicate");
+
+      const replicate = new Replicate({
+        auth: REPLICATE_API_KEY,
+      });
+
+      const prompt = interaction.options.getString("prompt");
+      const model = interaction.options.getString("model") || models[0].value;
+
+      const output = await replicate.run(model, { input: { prompt } });
+
+      const row = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setLabel(`Download`)
+          .setStyle(ButtonStyle.Link)
+          .setURL(`${output[0]}`)
+          .setEmoji("1101133529607327764")
+      );
+
+      const resultEmbed = new EmbedBuilder()
+        .setTitle("Imagem Gerada")
+        .addFields({ name: "Prompt", value: prompt })
+        .setImage(output[0])
+        .setColor("#44a3e3")
+        .setFooter({
+          text: `Requested by ${interaction.user.username}`,
+          iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+        });
+
+      await interaction.editReply({
+        embeds: [resultEmbed],
+        components: [row],
+      });
+    } catch (error) {
+      const errEmbed = new EmbedBuilder()
+        .setTitle("Ops deu erro")
+        .setDescription("```" + error + "```")
+        .setColor(0xe32424);
+
+      interaction.editReply({ embeds: [errEmbed] });
+    }
+  },
+
+  data: {
+    name: "imagine",
+    description: "Generate an image using a prompt.",
+    options: [
+      {
+        name: "prompt",
+        description: "Enter your prompt",
+        type: ApplicationCommandOptionType.String,
+        required: true,
+      },
+      {
+        name: "model",
+        description: "The image model",
+        type: ApplicationCommandOptionType.String,
+        choices: models,
+        required: false,
+      },
+    ],
+  },
+};
